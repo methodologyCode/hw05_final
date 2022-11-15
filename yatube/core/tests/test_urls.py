7b3bb015198a74b7c6a7bbf84276689a58026336
Александр Ш <method4354@yandex.ru>
2022-11-15 20:39:28 +0500

    final

--- yatube/core/tests/test_urls.py
+++ yatube/core/tests/test_urls.py
@@ -1,6 +1,12 @@
-def test_template_url_create(self):
-    """Проверка кастомного шаблона 404."""
-    url = '/unexisting_page/'
-    template = 'core/404.html'
-    response = self.authorized_client.get(url)
-    self.assertTemplateUsed(response, template)
+from django.test import TestCase, Client
+
+
+class CoreURLTests(TestCase):
+    def setUp(self):
+        self.guest_client = Client()
+
+    def test_template_url_create(self):
+        url = '/unexisting_page/'
+        template = 'core/404.html'
+        response = self.guest_client.get(url)
+        self.assertTemplateUsed(response, template)
